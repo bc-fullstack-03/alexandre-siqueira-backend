46e3716103223b8d19346dc249e4b60761bf89be
aspli <alexandre.xoquito@gmail.com>
2023-05-09 11:04:34 -0300

    Initial commit

---
+++ src/main/java/com/br/aspli/alexandresiqueirabackend/AlexandreSiqueiraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.aspli.alexandresiqueirabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandreSiqueiraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandreSiqueiraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/aspli/alexandresiqueirabackend/AlexandreSiqueiraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.aspli.alexandresiqueirabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandreSiqueiraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
